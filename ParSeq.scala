0de3364c9ce2c005d7805b2302d4a9ff9fe8ad9f
inthra.onsap <inthra.onsap@gmail.com>
2017-10-25 22:02:49 +0700

    Implement parallel scanLeft and scanRight

--- ParSeq.scala
+++ ParSeq.scala
@@ -22,32 +22,119 @@ object ParSeq {
     }
   }
 
-  private def seqProcessTransformFunc[A](in: Array[A], from: Int, until: Int)(transformFunc: (A, A) => A): A = {
+  private def seqProcessCombineFunc[A](in: Array[A], from: Int, until: Int)(combineFunc: (A, A) => A): A = {
     if ((until - from) == 1) in(from)
-    else transformFunc(in(from), seqProcessTransformFunc(in, from + 1, until)(transformFunc))
+    else combineFunc(in(from), seqProcessCombineFunc(in, from + 1, until)(combineFunc))
   }
 
-  private def fold[A](in: Array[A], from: Int, until: Int)(transformFunc: (A, A) => A): A = {
+  private def fold[A](in: Array[A], from: Int, until: Int)(combineFunc: (A, A) => A): A = {
     if ((until - from) < threshold) {
-      seqProcessTransformFunc(in, from, until)(transformFunc)
+      seqProcessCombineFunc(in, from, until)(combineFunc)
     } else {
       var mid = (from + until) / 2
-      val (left, right) = parallel(fold(in, from, mid)(transformFunc), fold(in, mid, until)(transformFunc))
-      transformFunc(left, right)
+      val (left, right) = parallel(fold(in, from, mid)(combineFunc), fold(in, mid, until)(combineFunc))
+      combineFunc(left, right)
     }
   }
 
-  def parFold[A](in: Array[A], initVal: A)(transformFunc: (A, A) => A): A = {
+  def parFold[A](in: Array[A], initVal: A)(combineFunc: (A, A) => A): A = {
     if (in.length == 0) initVal
-    else transformFunc(initVal, fold(in, 0, in.length)(transformFunc))
+    else combineFunc(initVal, fold(in, 0, in.length)(combineFunc))
   }
 
-  def parReduce[A](in: Array[A])(transformFunc: (A, A) => A): A = {
+  def parReduce[A](in: Array[A])(combineFunc: (A, A) => A): A = {
     require(in.length > 1, "Required Array conntains more than 1 element")
-    fold(in, 0, in.length)(transformFunc)
+    fold(in, 0, in.length)(combineFunc)
   }
 
-  def parScan[A](input: Array[A], a0: A, func: (A, A) => A, output: Array[A]): Unit = ???
 
+  sealed abstract class Tree[A]
 
+  case class Leaf[A](value: A) extends Tree[A]
+
+  case class Node[A](left: Tree[A], right: Tree[A], value: A) extends Tree[A]
+
+  sealed abstract class ResTree[A]
+
+  case class ResLeaf[A](value: A) extends ResTree[A]
+
+  case class ResNode[A](left: ResTree[A], right: ResTree[A]) extends ResTree[A]
+
+  private def buildTreeFromArray[A](in: Array[A], from: Int, until: Int)(combineFunc: (A, A) => A): Tree[A] = {
+    if ((until - from) == 1) {
+      Leaf(in(from))
+    } else {
+      val mid = (until + from) / 2
+      val (left, right) = parallel(buildTreeFromArray(in, from, mid)(combineFunc), buildTreeFromArray(in, mid, until)(combineFunc))
+
+      (left, right) match {
+        case (Leaf(v1), Leaf(v2)) => Node(left, right, combineFunc(v1, v2))
+        case (Leaf(v1), Node(_, _, v2)) => Node(left, right, combineFunc(v1, v2))
+        case (Node(_, _, v1), Leaf(v2)) => Node(left, right, combineFunc(v1, v2))
+        case (Node(_, _, v1), Node(_, _, v2)) => Node(left, right, combineFunc(v1, v2))
+      }
+    }
+  }
+
+  private def buildLeftistTree[A](tree: Tree[A], a0: A)(combineFunc: (A, A) => A): ResTree[A] = tree match {
+    case Leaf(v) => ResLeaf(combineFunc(a0, v))
+    case Node(left, right, v) => {
+      val lValue = left match {
+        case Node(_, _, v) => v
+        case Leaf(v) => v
+      }
+      val (l, r) = parallel(
+        buildLeftistTree(left, a0)(combineFunc),
+        buildLeftistTree(right, combineFunc(a0, lValue))(combineFunc)
+      )
+      ResNode(l, r)
+    }
+  }
+
+  private def buildRightistTree[A](tree: Tree[A], a0: A)(combineFunc: (A, A) => A): ResTree[A] = tree match {
+    case Leaf(v) => ResLeaf(combineFunc(v, a0))
+    case Node(left, right, v) => {
+      val rValue = right match {
+        case Node(_, _, v) => v
+        case Leaf(v) => v
+      }
+      val (l, r) = parallel(
+        buildRightistTree(left, combineFunc(a0, rValue))(combineFunc),
+        buildRightistTree(right, a0)(combineFunc)
+      )
+      ResNode(l, r)
+    }
+  }
+
+  private def buildArrayFromTree[A: Manifest](tree: ResTree[A]): Array[A] = tree match {
+    case ResLeaf(v) => Array(v)
+    case ResNode(l, r) => {
+      val (leftArr, rightArr) = parallel(buildArrayFromTree(l), buildArrayFromTree(r))
+      leftArr ++ rightArr
+    }
+  }
+
+  def parScanLeft[A: Manifest](in: Array[A], a0: A)(combineFunc: (A, A) => A): Array[A] = {
+    if (in.length == 0)
+      Array(a0)
+    else
+      a0 +: buildArrayFromTree(
+        buildLeftistTree(
+          buildTreeFromArray(in, 0, in.length)(combineFunc),
+          a0
+        )(combineFunc)
+      )
+  }
+
+  def parScanRight[A: Manifest](in: Array[A], a0: A)(combineFunc: (A, A) => A): Array[A] = {
+    if (in.length == 0)
+      Array(a0)
+    else
+      buildArrayFromTree(
+        buildRightistTree(
+          buildTreeFromArray(in, 0, in.length)(combineFunc),
+          a0
+        )(combineFunc)
+      ) :+ a0
+  }
 }
--- README.md
+++ README.md
@@ -3,10 +3,10 @@
 A sample code implementation for merge sort, map, reduce, fold and scan in parallel scheme.
  
 
-### Usage Examples
+## Usage Examples
 
 
-##### ParOrdering Object
+### ParOrdering Object
 
 ```sbtshell
 scala> import ionsap.util._
@@ -40,7 +40,7 @@ res2: Array[Int] = Array(-2, 0, 0, 0, 0, 2, 2, 5, 8, 8, 8, 9, 9, 9, 17, 89, 900)
 ```
 
 
-##### ParSeq Object
+### ParSeq Object
 ```sbtshell
 scala> import ionsap.util._
 
@@ -56,15 +56,24 @@ res1: Array[Double] = Array(1.5, 5.5, 78.5, 9.5, 0.5, 7.5, 3.5, 1.5, 5.5, 7.5, 8
 /**
 * Parallel Fold utility
 */
-scala> ParSeq.parFold(in, 0)((acc, x) => acc + x)
-res2:Int = 212
+scala> val initVal:Int = 0
+initVal: Int = 0
 
+scala> ParSeq.parFold(in, initVal)((acc, x) => acc + x)
+res2: Int = 212
 
 /**
 * Parallel Reduce utility
 */
-scala> ParSeq.parReduce(in, 0)((acc, x) => acc + x)
-res3:Int = 212
+scala> ParSeq.parReduce(in)((acc, x) => acc + x)
+res3: Int = 212
 
+/**
+* Parallel ReduceLeft & ReduceRight utility
+*/
+scala> ParSeq.parScanLeft(in, 5)((acc, x) => acc + x)
+res4: Array[Int] = Array(5, 6, 11, 89, 98, 98, 105, 108, 109, 114, 121, 203, 212, 217)
 
+scala> ParSeq.parScanRight(in, 5)((x, acc) => x + acc)
+res5: Array[Int] = Array(217, 216, 211, 133, 124, 124, 117, 114, 113, 108, 101, 19, 10, 5)
 ```
